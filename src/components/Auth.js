f450de750a5de424a0279788f68b09b6480a5190
Victor Song <vms2@rice.edu>
2020-06-11 23:44:19 -0500

    Tried useFetch instead

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,40 +1,9 @@
 import React from "react";
 import { Redirect } from "react-router-dom";
 
-import { useAsync } from "react-async";
+import { useFetch } from "react-async";
 
 import { BACKEND_AUTH_URL } from "../config";
-
-async function doBackendAuth({ query }, { signal }) {
-    let response = await fetch(BACKEND_AUTH_URL, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(query),
-        signal: signal,
-    });
-
-    console.log(response);
-
-    if (!response.ok) {
-        console.log("Not okay!");
-        throw new Error(response.status);
-    }
-
-    const data = await response.json();
-
-    console.log(data);
-
-    if (!data.success) {
-        console.log("No success!");
-        throw new Error(data.success);
-    }
-
-    localStorage.setItem("RICE_DISCUSS_TOKEN", data.user.token);
-
-    return data.user.netID;
-}
 
 const Auth = (successPath, errPath) => {
     const ticket = new URLSearchParams(window.location.search).get("ticket");
@@ -43,7 +12,10 @@ const Auth = (successPath, errPath) => {
         ticket: ticket,
     };
 
-    const { err } = useAsync({ promiseFn: doBackendAuth, query });
+    const { isPending, error, run } = useFetch(BACKEND_AUTH_URL, {
+        method: "POST",
+        body: JSON.stringify(query),
+    });
 
     return err ? (
         <Redirect to={`/${errPath}`} />
