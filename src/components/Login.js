0389baf1b9c1fd946013c0299bfdbf1b2e51a2d4
Bria Weisz <btw4@Brias-MacBook-Pro.local>
2020-06-28 20:55:49 -0400

    thank god for cinemascope bc that is the aspect ratio of this svg

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 
 import Auth from "./Auth";
 import { FRONTEND_AUTH_URL } from "../config";
-import LoginButton from "./Login.styles";
+// import LoginButton from "./Login.styles";
+import './Login.css';
 
 function Login() {
     let [hasTicket] = useState(
@@ -12,14 +13,17 @@ function Login() {
     return hasTicket ? (
         Auth("discussions", "login")
     ) : (
-        <div style={{ height: "100vh", width: "100vw", textAlign: "center" }}>
-            <LoginButton
+        <div className = 'container'>
+            <div className = 'login-image'>
+            </div>
+            <button className = 'login-button'
                 onClick={() => {
                     window.open(FRONTEND_AUTH_URL, "_self");
                 }}
             >
                 Login
-            </LoginButton>
+            </button>
+            
         </div>
     );
 }
