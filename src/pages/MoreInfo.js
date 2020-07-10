95fa2c207f1f0a3dc90c17877badf0040b0b0b6b
Brandon Zhang <brandonzhang00@gmail.com>
2020-07-10 19:06:31 -0400

    Updated moreinfo page

--- src/pages/MoreInfo.js
+++ src/pages/MoreInfo.js
@@ -4,53 +4,77 @@ import { useMutation } from "@apollo/react-hooks";
 import { TOKEN_NAME } from "../utils/config";
 import { SET_INFO} from "../graphql/Mutations";
 import laptop_girl from "../images/Page 2.svg"
+import major_minor_json from "./MajorMinor.json"
+import DropDownItem from "../components/DropDownItem.js"
 import './MoreInfo.styles.css';
 
 const MoreInfo = () => {
     const [loading, setLoading] = useState(false);
     const [username, setUsername] = useState("");
-
-    const [major, setMajor] = useState("");
-    const [minor, setMinor] = useState("")
+    const [major, setMajor] = useState([]);
+    const [minor, setMinor] = useState([])
     const [college, setCollege] = useState("");
-    const [grad, setGrad] = useState("");
+    const [isMajorOpen, setMajorOpen] = useState(false);
+    const [isMinorOpen, setMinorOpen] = useState(false);
+    const [isCollegeOpen, setCollegeOpen] = useState(false);
+
     const [addInfo, {response}] = useMutation(SET_INFO);
-    const data = JSON.parse(localStorage.getItem(TOKEN_NAME))
+    const data = JSON.parse(localStorage.getItem(TOKEN_NAME));
+
+    const majors = major_minor_json.majors.split(";").map(major => {
+        const major_obj = {
+          name: major,
+        }
+        return major_obj;
+    });
+
+    const minors = major_minor_json.minors.split(";").map(minor => {
+      const minor_obj = {
+        name: minor,
+      }
+      return minor_obj;
+    });
+
+    const colleges = major_minor_json.colleges.split(";");
 
     const handleUserChange = useCallback(
         (e) => setUsername(e.target.value), []
     )
 
-    const handleMajorChange = useCallback(
-        (e) => setMajor(e.target.value), []
-    )
+    const handleMajorChange = newValue => { 
+      const index_of_major = major.indexOf(newValue)
+      setMajor(index_of_major >= 0 ? major.filter(maj => newValue !== maj) : [...major, newValue]);
+    }
 
-    const handleMinorChange = useCallback(
-      (e) => setMinor(e.target.value), []
-  )
+    const handleMinorChange = newValue => {
+      const index_of_minor = minor.indexOf(newValue)
+      setMinor(index_of_minor >= 0 ? minor.filter(maj => newValue !== maj) : [...minor, newValue]);
+    }
 
-    const handleCollegeChange = useCallback(
-        (e) => setCollege(e.target.value), []
-    )
+    const handleCollegeChange = newValue => {
+      const index_of_college = college.indexOf(newValue)
+      setCollege(index_of_college >= 0 ? "" : newValue);
+    }
+
+    const toggleMajor = () => setMajorOpen(!isMajorOpen);
+    const toggleMinor = () => setMinorOpen(!isMinorOpen);
+    const toggleCollege = () => setCollegeOpen(!isCollegeOpen);
+    const majorArrow = isMajorOpen ? "arrow up" : "arrow down";
+    const minorArrow = isMinorOpen ? "arrow up" : "arrow down";
+    const collegeArrow = isCollegeOpen ? "arrow up" : "arrow down";
 
     if (!data?.isNewUser){
       console.log("Redirecting....")
       return ( <Redirect to={"/discussions"} />);
     }
 
-    //we have to post stuff to backend.  We're not doing a handle submit until all
-    //forms get activated, testing for now...
-    //we mutate our data with a custom mutation to the backend using graphql
-    //Add in SVG later
     const handleSubmit = async (e) => {
         e.preventDefault();
         setLoading(true);
-        const majorArray = [major];
-        const minorArray = [minor];
         try{
           console.log({
-            "major": majorArray,
-            "minor": minorArray,
+            "major": major.toString(),
+            "minor": minor.toString(),
             "college": college,
             "netID": data.netID,
             "username": username,
@@ -63,7 +87,7 @@ const MoreInfo = () => {
               major: major,
               minor: minor,
               netID: data.netID,
-              isNewUser: false,
+              isNewUser: false, 
           },
           });
 
@@ -89,8 +113,7 @@ const MoreInfo = () => {
 
         <form onSubmit={handleSubmit} className = "margins">
         <fieldset className ="totalform">
-          <fieldset className="textfield">
-            <label for="uname">Username</label> <br/>
+          <fieldset className="fieldsetstyle">
             <input
               className="textfield"
               type="text"
@@ -100,38 +123,59 @@ const MoreInfo = () => {
             />
           </fieldset>
 
-          <fieldset className="textfield">
-            <label for="major">Major</label> <br/>
-            <input
-              className="textfield"
-              type="text"
-              placeholder="major"
-              value={major}
-              onChange={handleMajorChange}
-            />
-          </fieldset>
+          <div className="dd-wrapper">
+            <div className="dd-header" onClick={toggleMajor}>
+                    <div className="dd-header-title">Majors
+                      <i className = {majorArrow}/>
+                    </div>
+            </div>
+            {isMajorOpen && <ul className="dd-list">
+              {majors.map(item => (
+                  <li className="dd-list-item" key={item.name}>
+                    <DropDownItem name={item.name} setInfo = {handleMajorChange}
+                    selectedItems = {major}/>
+                  </li>
+                ))
+              }
+              </ul>
+            }
+          </div>
 
-          <fieldset className="textfield">
-            <label for="major">Minor</label> <br/>
-            <input
-              className="textfield"
-              type="text"
-              placeholder="minor"
-              value={minor}
-              onChange={handleMinorChange}
-            />
-          </fieldset>
+          <div className="dd-wrapper">
+            <div className="dd-header" onClick={toggleMinor}>
+                    <div className="dd-header-title">Minors
+                      <i className = {minorArrow}/>
+                    </div>
+            </div>
+            {isMinorOpen && <ul className="dd-list">
+              {minors.map(item => (
+                  <li className="dd-list-item" key={item.name}>
+                    <DropDownItem name={item.name} setInfo = {handleMinorChange}
+                    selectedItems = {minor}/>
+                  </li>
+                ))
+              }
+              </ul>
+            }
+          </div>
 
-          <fieldset className="textfield">
-            <label for="college">College</label> <br/>
-            <input
-              className="textfield"
-              type="text"
-              placeholder="college"
-              value={college}
-              onChange={handleCollegeChange}
-            />
-          </fieldset>
+          <div className="dd-wrapper">
+            <div className="dd-header" onClick={toggleCollege}>
+                    <div className="dd-header-title">{college === "" ? "College" : college}
+                      <i className = {collegeArrow}/>
+                    </div>
+            </div>
+            {isCollegeOpen && <ul className="dd-list">
+              {colleges.map(item => (
+                  <li className="dd-list-item" key={item}>
+                    <DropDownItem name={item} setInfo = {handleCollegeChange}
+                    selectedItems = {college}/>
+                  </li>
+                ))
+              }
+              </ul>
+            }
+          </div>
           <button
             className="submitbutton"
             type="submit"
