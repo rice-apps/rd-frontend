0ad257448979dd3b475369c09ccd28b0689a6242
Victor Song <vms2@rice.edu>
2020-08-13 00:01:10 -0500

    Use service worker in production

--- src/index.jsx
+++ src/index.jsx
@@ -24,4 +24,9 @@ render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  console.log("DEVELOPMENT")
+  serviceWorker.unregister()
+} else {
+  serviceWorker.register()
+}
