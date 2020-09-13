8f4d15777a5f9965f4351a07c7047da3148b7dec
Henry Qin <hqin2400@gmail.com>
2020-09-12 20:45:42 -0500

    postchunk adjustments after pulling

--- src/components/PostChunk.jsx
+++ src/components/PostChunk.jsx
@@ -533,13 +533,13 @@ function PostChunk (props) {
                   }
                 }}
               >
-                {/* <CommentButtonText> */}
-                  {/* Post Comment */}
-                {/* </CommentButtonText> */}
+                <CommentButtonText>
+                  Post Comment
+                </CommentButtonText>
               </CommentButton>
             )}
             {isCommentOpen && (
-              // <CommentsDiv>
+              <CommentsDiv>
                 <ul style={{listStyleType:"none", paddingLeft:"0px"}}>
                   {/* level 1 */}
                   {theComments.map((comment) => (
@@ -569,7 +569,7 @@ function PostChunk (props) {
                     </li>
                   ))}
                 </ul>
-              // </CommentsDiv>
+              </CommentsDiv>
             )}
           </CommentComponent>
         </DiscussionBox>
